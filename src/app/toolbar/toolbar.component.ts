545930518d038f6f9a2b0d9c68934a73aeea8190
Johannes Weih <sheepr4ider@gmail.com>
2022-09-27 19:26:53 +0200

    feat: added github button

--- src/app/toolbar/toolbar.component.ts
+++ src/app/toolbar/toolbar.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
 import { Observable } from 'rxjs';
 import { AuthService } from '../auth/auth.service';
 
@@ -13,11 +15,22 @@ export class ToolbarComponent {
   }
 
   public constructor(
-    private authSv: AuthService
-  ) { }
+    private authSv: AuthService,
+    private matIconRegistry: MatIconRegistry,
+    private domSanitizer: DomSanitizer
+  ) {
+    this.matIconRegistry.addSvgIcon(
+      'github',
+      this.domSanitizer.bypassSecurityTrustResourceUrl('assets/github.svg')
+    );
+  }
 
   public signOut(): void {
     this.authSv.signOut();
+  }
+
+  public navigateToRepo(): void {
+    window.open('https://github.com/xpl0t/playlist-dealer', '_blank');
   }
 
 }
